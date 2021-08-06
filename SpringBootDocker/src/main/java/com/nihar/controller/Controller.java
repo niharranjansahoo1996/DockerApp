2857f1e89517b67693d15221d4151f57ebf2115a
TL <TL@gmail.com>
2021-08-06 09:53:22 -0500

    1st commit

---
+++ SpringBootDocker/src/main/java/com/nihar/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.nihar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/msg")
+	public String getmsg() {
+
+		return "Hello World!!";
+	}
+
+}
---
+++ SpringBootDocker/src/main/java/com/nihar/pojo/Employee.java
@@ -0,0 +1,7 @@
+package com.nihar.pojo;
+
+public class Employee {
+
+	Integer id;
+	String name;
+}
